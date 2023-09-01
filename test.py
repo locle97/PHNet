fae11ba7164e0d41c92f451bf2e2a9527cdae781
befozg <kkefremyan@sberbank.ru>
2023-09-01 14:44:57 +0300

    added test script, fixed conda environment

--- test.py
+++ test.py
@@ -1,14 +1,36 @@
-from trainer import Trainer, Inference
+from trainer import Inference
 from omegaconf import OmegaConf
-from torch import multiprocessing as mp
-from torch import distributed as dist
 import os
 import sys
+import PIL
+from matplotlib import pyplot as plt
+import numpy as np
+import torchvision.transforms.functional as tf
+
+def predict(comp, mask):
+    comp = comp.convert('RGB')
+    mask = mask.convert('1')
+    in_shape = comp.size[::-1]
+    image_size = args.input.image_size
+    comp = tf.resize(comp, [image_size, image_size])
+    mask = tf.resize(mask, [image_size, image_size])
         
+    compt = tf.to_tensor(comp)
+    maskt = tf.to_tensor(mask)
+    res = infer.harmonize(compt, maskt)
+    res = tf.resize(res, in_shape)
+
+    return np.uint8((res*255)[0].permute(1,2,0).numpy())
+
 if __name__ == '__main__':
 
     args = OmegaConf.load(os.path.join(os.path.dirname(
         os.path.abspath(__file__)), f"config/test.yaml"))
     
     infer = Inference(**args)
+    comp = PIL.Image.open(args.input.composite_path)
+    mask = PIL.Image.open(args.input.mask_path)
+
+    harmonized = predict(comp, mask)
+    plt.imsave(args.output.path, harmonized)
     
